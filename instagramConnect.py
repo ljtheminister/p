366b7f0ae119783a17654ff8678deb02346f53d8
ljtheminister <john.c.min@gmail.com>
2014-01-03 15:50:49 +0000

    jan 3 2014 commit

--- instagramConnect.py
+++ instagramConnect.py
@@ -4,16 +4,21 @@ from instagram.client import InstagramAPI
 client_id = '54e559fad6dc4baf86127cc8baa9a0dd'
 client_secret = 'ee8bb41e2f434f32934db68e379d7b91'
 redirect_uri = 'http://localhost'
+callback_url = 'http://myhost.com/test/start.php'
 
+api = InstagramAPI(client_id=client_id, client_secret=client_secret)
+
+tag = 'occupynigeria'
+api.create_subscription(object='tag', object_id=tag, aspect='media', callback_url=callback_url)
 
 access_token = 'MY ACCESS TOKEN'
 api = InstagramAPI(access_token=access_token)
 
 
+
 new_api = client.InstagramAPI(client_id='', client_secret='', redirect_uri='http://ontodjangoyo.herokuapp.com/oauth_callback')
 
 new_api.create_subscription(object='tag', object_id='ed', aspect='media', callback_url='http://ontodjangoyo.herokuapp.com/instag/realtime_callback/')
-
 
 api.create_subscription(object='tag', object_id='fox', aspect='media', callback_url='http://mydomain.com/hook/instagram')
 
--- settings.py
+++ settings.py
@@ -15,5 +15,16 @@ TWITTER_CONSUMER_SECRET     = 'T3wrxLEvrXMEP5duYivjAoEVukfoJXLZZg71jIW0SU'
 TWITTER_ACCESS_TOKEN        = '1619689098-4Stfk3mTi1lBGWkG3qoTaY6C5vDup0IZqGeCc7E'
 TWITTER_ACCESS_TOKEN_SECRET = 'iIN3sC5HQ4kiydgmAqbDfvyldQwJPptQk7YRsBHfbEVDJ'
 
+
+'''
+Instagram
+Account: ljtheminister
+Application:  instagram_api
+'''
+INSTAGRAM_CLIENT_ID = '54e559fad6dc4baf86127cc8baa9a0dd'
+INSTAGRAM_CLIENT_SECRET = 'ee8bb41e2f434f32934db68e379d7b91'
+
+
+
 '''Twitter Keywords'''
 KEYWORDS = ['#tahrir', 'تحرير']
